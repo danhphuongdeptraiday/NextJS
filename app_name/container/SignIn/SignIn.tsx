3bd7b6e9972fbf80958c3bd539bac0a02130d04a
Nguyễn Danh Phương <nguyendanhphuong0305@gmail.com>
2022-10-03 19:11:51 +0700

    new commit

--- app_name/container/SignIn/SignIn.tsx
+++ app_name/container/SignIn/SignIn.tsx
@@ -1,30 +1,24 @@
-import React from 'react'
-import classes from "./SignIn.module.scss"
-import Title from '../../components/Title/Title'
-import Button from '../../components/Button/Button'
-import { useRouter } from 'next/router'
-
+import React from "react";
+import classes from "./SignIn.module.scss";
+import Title from "components/Title/Title";
+import Button from "components/Button/Button";
+import { useRouter } from "next/router";
 
 function SignIn() {
+  const router = useRouter();
 
-    const router = useRouter()
-
-    const goToSignUp = () => {
-        // console.log("Go into SignUp");
-        router.push("/signup")
-        
-    }
-
+  const goToSignUp = () => {
+    // console.log("Go into SignUp");
+    router.push("/signup");
+  };
 
   return (
     <div className={classes.container}>
-        <Title >
-            Sign In My App
-        </Title>
+      <Title>Sign In My App</Title>
 
-        <Button onClick={goToSignUp}>Go TO Sign Up</Button>
+      <Button onClick={goToSignUp}>Go TO Sign Up</Button>
     </div>
-  )
+  );
 }
 
-export default SignIn
+export default SignIn;
